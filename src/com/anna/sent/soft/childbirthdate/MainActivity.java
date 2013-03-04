1d178e7b0e09e5ef485898b66c4dbaf615137b16
anna sent <anna.sent@gmail.com>
2013-03-04 20:18:21 +0400

    save and restore settings

--- src/com/anna/sent/soft/childbirthdate/MainActivity.java
+++ src/com/anna/sent/soft/childbirthdate/MainActivity.java
@@ -3,7 +3,10 @@ package com.anna.sent.soft.childbirthdate;
 import java.util.Calendar;
 
 import android.app.TabActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
@@ -24,6 +27,29 @@ public class MainActivity extends TabActivity {
 	private DatePicker datePickerLastMenstruationDate, datePickerOvulationDate,
 			datePickerUltrasoundDate;
 	private RadioButton radioButtonObstetic;
+
+	public static final String EXTRA_MENSTRUAL_CYCLE_LEN = "com.anna.sent.soft.childbirthdate.menstrualcyclelen";
+	public static final String EXTRA_LUTEAL_PHASE_LEN = "com.anna.sent.soft.childbirthdate.lutealphaselen";
+	public static final String EXTRA_BY_LAST_MENSTRUATION_DATE = "com.anna.sent.soft.childbirthdate.bylastmenstruationdate";
+	public static final String EXTRA_LAST_MENSTRUATION_DATE = "com.anna.sent.soft.childbirthdate.lastmenstruationdate";
+	public static final String EXTRA_BY_OVULATION_DATE = "com.anna.sent.soft.childbirthdate.byovulationdate";
+	public static final String EXTRA_OVULATION_DATE = "com.anna.sent.soft.childbirthdate.ovulationdate";
+	public static final String EXTRA_BY_ULTRASOUND = "com.anna.sent.soft.childbirthdate.byultrasound";
+	public static final String EXTRA_ULTRASOUND_DATE = "com.anna.sent.soft.childbirthdate.ultrasounddate";
+	public static final String EXTRA_WEEKS = "com.anna.sent.soft.childbirthdate.weeks";
+	public static final String EXTRA_DAYS = "com.anna.sent.soft.childbirthdate.days";
+	public static final String EXTRA_IS_FETAL = "com.anna.sent.soft.childbirthdate.isfetal";
+
+	public static final String SETTINGS_FILE = "childbirthdatesettings";
+
+	private int menstrualCycleLen;
+	private int lutealPhaseLen;
+	private Calendar lastMenstruationDate = Calendar.getInstance();
+	private Calendar ovulationDate = Calendar.getInstance();
+	private Calendar ultrasoundDate = Calendar.getInstance();
+	private int weeks;
+	private int days;
+	private boolean isFetal;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -53,48 +79,86 @@ public class MainActivity extends TabActivity {
 		numberPcikerLutealPhaseLen
 				.setMaxValue(ChildbirthDateCalculator.MAX_LUTEAL_PHASE_LEN);
 
-		restoreDefaultValues(null);
-
 		checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
 		linearLayout1 = (LinearLayout) findViewById(R.id.linearLayout1);
-		checkVisibility(checkBox1, linearLayout1);
 		datePickerLastMenstruationDate = (DatePicker) findViewById(R.id.datePickerLastMenstruationDate);
 
 		checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
 		linearLayout2 = (LinearLayout) findViewById(R.id.linearLayout2);
-		checkVisibility(checkBox2, linearLayout2);
 		datePickerOvulationDate = (DatePicker) findViewById(R.id.datePickerOvulationDate);
 
 		checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
 		linearLayout3 = (LinearLayout) findViewById(R.id.linearLayout3);
-		checkVisibility(checkBox3, linearLayout3);
 		datePickerUltrasoundDate = (DatePicker) findViewById(R.id.datePickerUltrasoundDate);
 
 		radioButtonObstetic = (RadioButton) findViewById(R.id.radioObstetic);
 
 		numberPickerWeeks = (NumberPicker) findViewById(R.id.editTextWeeks);
 		numberPickerWeeks.setMinValue(0);
-		radioClick(null);
-		numberPickerWeeks.setValue(0);
 
 		numberPickerDays = (NumberPicker) findViewById(R.id.editTextDays);
 		numberPickerDays.setMinValue(0);
 		numberPickerDays
 				.setMaxValue(ChildbirthDateCalculator.DAYS_IN_A_WEEK - 1);
-		numberPickerDays.setValue(0);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		SharedPreferences settings = getApplicationContext()
+				.getSharedPreferences(SETTINGS_FILE, Context.MODE_PRIVATE);
+		menstrualCycleLen = settings.getInt(EXTRA_MENSTRUAL_CYCLE_LEN,
+				ChildbirthDateCalculator.MENSTRUAL_CYCLE_AVG_LENGTH);
+		lutealPhaseLen = settings.getInt(EXTRA_LUTEAL_PHASE_LEN,
+				ChildbirthDateCalculator.LUTEAL_PHASE_AVG_LENGTH);
+		checkBox1.setChecked(settings.getBoolean(
+				EXTRA_BY_LAST_MENSTRUATION_DATE, false));
+		lastMenstruationDate.setTimeInMillis(settings.getLong(
+				EXTRA_LAST_MENSTRUATION_DATE, System.currentTimeMillis()));
+		checkBox2.setChecked(settings
+				.getBoolean(EXTRA_BY_OVULATION_DATE, false));
+		ovulationDate.setTimeInMillis(settings.getLong(EXTRA_OVULATION_DATE,
+				System.currentTimeMillis()));
+		checkBox3.setChecked(settings.getBoolean(EXTRA_BY_ULTRASOUND, false));
+		ultrasoundDate.setTimeInMillis(settings.getLong(EXTRA_ULTRASOUND_DATE,
+				System.currentTimeMillis()));
+		weeks = settings.getInt(EXTRA_WEEKS, 0);
+		days = settings.getInt(EXTRA_DAYS, 0);
+		isFetal = settings.getBoolean(EXTRA_IS_FETAL, isFetal);
+		writeValues();
+		checkVisibility(checkBox1, linearLayout1);
+		checkVisibility(checkBox2, linearLayout2);
+		checkVisibility(checkBox3, linearLayout3);
+		radioClick(null);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		SharedPreferences settings = getApplicationContext()
+				.getSharedPreferences(SETTINGS_FILE, Context.MODE_PRIVATE);
+		Editor editor = settings.edit();
+		readValues();
+		editor.putInt(EXTRA_MENSTRUAL_CYCLE_LEN, menstrualCycleLen);
+		editor.putInt(EXTRA_LUTEAL_PHASE_LEN, lutealPhaseLen);
+		editor.putBoolean(EXTRA_BY_LAST_MENSTRUATION_DATE,
+				checkBox1.isChecked());
+		editor.putLong(EXTRA_LAST_MENSTRUATION_DATE,
+				lastMenstruationDate.getTimeInMillis());
+		editor.putBoolean(EXTRA_BY_OVULATION_DATE, checkBox2.isChecked());
+		editor.putLong(EXTRA_OVULATION_DATE, ovulationDate.getTimeInMillis());
+		editor.putBoolean(EXTRA_BY_ULTRASOUND, checkBox3.isChecked());
+		editor.putLong(EXTRA_ULTRASOUND_DATE, ultrasoundDate.getTimeInMillis());
+		editor.putInt(EXTRA_WEEKS, weeks);
+		editor.putInt(EXTRA_DAYS, days);
+		editor.putBoolean(EXTRA_IS_FETAL, isFetal);
+		editor.commit();
 	}
 
 	public void radioClick(View view) {
 		numberPickerWeeks
 				.setMaxValue((radioButtonObstetic.isChecked() ? ChildbirthDateCalculator.OBSTETIC_PREGNANCY_PERIOD
 						: ChildbirthDateCalculator.FETAL_PREGNANCY_PERIOD) - 1);
-	}
-
-	public void restoreDefaultValues(View view) {
-		numberPickerMenstrualCycleLen
-				.setValue(ChildbirthDateCalculator.MENSTRUAL_CYCLE_AVG_LENGTH);
-		numberPcikerLutealPhaseLen
-				.setValue(ChildbirthDateCalculator.LUTEAL_PHASE_AVG_LENGTH);
 	}
 
 	private Calendar getDate(DatePicker datePicker) {
@@ -104,28 +168,35 @@ public class MainActivity extends TabActivity {
 		return date;
 	}
 
-	public static final String EXTRA_MENSTRUAL_CYCLE_LEN = "com.anna.sent.soft.childbirthdate.menstrualcyclelen";
-	public static final String EXTRA_LUTEAL_PHASE_LEN = "com.anna.sent.soft.childbirthdate.lutealphaselen";
-	public static final String EXTRA_BY_LAST_MENSTRUATION_DATE = "com.anna.sent.soft.childbirthdate.bylastmenstruationdate";
-	public static final String EXTRA_LAST_MENSTRUATION_DATE = "com.anna.sent.soft.childbirthdate.lastmenstruationdate";
-	public static final String EXTRA_BY_OVULATION_DATE = "com.anna.sent.soft.childbirthdate.byovulationdate";
-	public static final String EXTRA_OVULATION_DATE = "com.anna.sent.soft.childbirthdate.ovulationdate";
-	public static final String EXTRA_BY_ULTRASOUND = "com.anna.sent.soft.childbirthdate.byultrasound";
-	public static final String EXTRA_ULTRASOUND_DATE = "com.anna.sent.soft.childbirthdate.ultrasounddate";
-	public static final String EXTRA_WEEKS = "com.anna.sent.soft.childbirthdate.weeks";
-	public static final String EXTRA_DAYS = "com.anna.sent.soft.childbirthdate.days";
-	public static final String EXTRA_IS_FETAL = "com.anna.sent.soft.childbirthdate.isfetal";
+	private void readValues() {
+		menstrualCycleLen = numberPickerMenstrualCycleLen.getValue();
+		lutealPhaseLen = numberPcikerLutealPhaseLen.getValue();
+		lastMenstruationDate = getDate(datePickerLastMenstruationDate);
+		ovulationDate = getDate(datePickerOvulationDate);
+		ultrasoundDate = getDate(datePickerUltrasoundDate);
+		weeks = numberPickerWeeks.getValue();
+		days = numberPickerDays.getValue();
+		isFetal = !radioButtonObstetic.isChecked();
+	}
+
+	private void setDate(DatePicker datePicker, Calendar date) {
+		datePicker.updateDate(date.get(Calendar.YEAR),
+				date.get(Calendar.MONTH), date.get(Calendar.DAY_OF_MONTH));
+	}
+
+	private void writeValues() {
+		numberPickerMenstrualCycleLen.setValue(menstrualCycleLen);
+		numberPcikerLutealPhaseLen.setValue(lutealPhaseLen);
+		setDate(datePickerLastMenstruationDate, lastMenstruationDate);
+		setDate(datePickerOvulationDate, ovulationDate);
+		setDate(datePickerUltrasoundDate, ultrasoundDate);
+		numberPickerWeeks.setValue(weeks);
+		numberPickerDays.setValue(days);
+		radioButtonObstetic.setChecked(!isFetal);
+	}
 
 	public void calculate(View view) {
-		int menstrualCycleLen = numberPickerMenstrualCycleLen.getValue();
-		int lutealPhaseLen = numberPcikerLutealPhaseLen.getValue();
-		Calendar lastMenstruationDate = getDate(datePickerLastMenstruationDate);
-		Calendar ovulationDate = getDate(datePickerOvulationDate);
-		Calendar ultrasoundDate = getDate(datePickerUltrasoundDate);
-		int weeks = numberPickerWeeks.getValue();
-		int days = numberPickerDays.getValue();
-		boolean isFetal = !radioButtonObstetic.isChecked();
-
+		readValues();
 		Intent intent = new Intent(this, ResultActivity.class);
 		intent.putExtra(EXTRA_MENSTRUAL_CYCLE_LEN, menstrualCycleLen);
 		intent.putExtra(EXTRA_LUTEAL_PHASE_LEN, lutealPhaseLen);
