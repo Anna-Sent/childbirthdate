9fd5e8963cd62a5bcf60cf34401cbcae59a7a40d
anna sent <anna.sent@gmail.com>
2013-04-16 17:46:48 +0400

    crash bug is fixed

--- src/com/anna/sent/soft/childbirthdate/fragments/CalculationPageLmpMethodFragment.java
+++ src/com/anna/sent/soft/childbirthdate/fragments/CalculationPageLmpMethodFragment.java
@@ -30,6 +30,17 @@ public class CalculationPageLmpMethodFragment extends DetailsFragment implements
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		if (container == null) {
+			// We have different layouts, and in one of them this
+			// fragment's containing frame doesn't exist. The fragment
+			// may still be created from its saved state, but there is
+			// no reason to try to create its view hierarchy because it
+			// won't be displayed. Note this is not needed -- we could
+			// just run the code below, where we would create and return
+			// the view hierarchy; it would just never be used.
+			return null;
+		}
+
 		View v = inflater
 				.inflate(R.layout.calculation_page_lmp_method_fragment,
 						container, false);
--- src/com/anna/sent/soft/childbirthdate/fragments/CalculationPageOvulationMethodFragment.java
+++ src/com/anna/sent/soft/childbirthdate/fragments/CalculationPageOvulationMethodFragment.java
@@ -23,9 +23,20 @@ public class CalculationPageOvulationMethodFragment extends DetailsFragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
+		if (container == null) {
+			// We have different layouts, and in one of them this
+			// fragment's containing frame doesn't exist. The fragment
+			// may still be created from its saved state, but there is
+			// no reason to try to create its view hierarchy because it
+			// won't be displayed. Note this is not needed -- we could
+			// just run the code below, where we would create and return
+			// the view hierarchy; it would just never be used.
+			return null;
+		}
+
 		View v = inflater.inflate(
-				R.layout.calculation_page_ovulation_method_fragment,
-				container, false);
+				R.layout.calculation_page_ovulation_method_fragment, container,
+				false);
 		return v;
 	}
 
