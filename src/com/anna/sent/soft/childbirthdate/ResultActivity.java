954684549b5c18452ec149a064b4706d01af0116
anna sent <anna.sent@gmail.com>
2013-04-23 08:16:08 +0400

    useless null-check

--- src/com/anna/sent/soft/childbirthdate/ResultActivity.java
+++ src/com/anna/sent/soft/childbirthdate/ResultActivity.java
@@ -70,8 +70,7 @@ public class ResultActivity extends ChildActivity {
 				textView0.setText(mContext
 						.getString(R.string.estimatedChildbirthDate));
 				textView00.setText(mContext.getString(R.string.rememberECD));
-			} else if (Shared.ResultParam.Calculate.EGA == whatToDo
-					&& currentDate != null) {
+			} else if (Shared.ResultParam.Calculate.EGA == whatToDo) {
 				textView0.setText(mContext
 						.getString(R.string.estimatedGestationAge)
 						+ " ("
@@ -103,31 +102,21 @@ public class ResultActivity extends ChildActivity {
 			if (byMethod[i]) {
 				switch (i) {
 				case 0:
-					if (lastMenstruationDate != null) {
-						pregnancies[i] = PregnancyCalculator.Factory.get(
-								lastMenstruationDate, menstrualCycleLen,
-								lutealPhaseLen);
-					}
-
+					pregnancies[i] = PregnancyCalculator.Factory.get(
+							lastMenstruationDate, menstrualCycleLen,
+							lutealPhaseLen);
 					break;
 				case 1:
-					if (ovulationDate != null) {
-						pregnancies[i] = PregnancyCalculator.Factory
-								.get(ovulationDate);
-					}
-
+					pregnancies[i] = PregnancyCalculator.Factory
+							.get(ovulationDate);
 					break;
 				case 2:
-					if (ultrasoundDate != null) {
-						pregnancies[i] = PregnancyCalculator.Factory.get(
-								ultrasoundDate, weeks, days, isEmbryonicAge);
-					}
-
+					pregnancies[i] = PregnancyCalculator.Factory.get(
+							ultrasoundDate, weeks, days, isEmbryonicAge);
 					break;
 				}
 
-				if (pregnancies[i] != null
-						&& whatToDo == Shared.ResultParam.Calculate.EGA) {
+				if (whatToDo == Shared.ResultParam.Calculate.EGA) {
 					pregnancies[i].setCurrentPoint(currentDate);
 				}
 
@@ -146,80 +135,51 @@ public class ResultActivity extends ChildActivity {
 	}
 
 	private void setEgaTexts(int i) {
-		if (results.length == 3 && messages.length == 3
-				&& pregnancies.length == 3 && i < 3 && i >= 0) {
-			TextView result = results[i], message = messages[i];
-			Pregnancy pregnancy = pregnancies[i];
-			if (pregnancy != null) {
-				Calendar start = pregnancy.getStartPoint();
-				Calendar end = pregnancy.getEndPoint();
-				if (start != null && end != null && currentDate != null) {
-					if (pregnancy.isCorrect()) {
-						if (result != null) {
-							result.setText(pregnancy.getInfo(mContext));
-						}
-						if (message != null) {
-							message.setText(pregnancy
-									.getAdditionalInfo(mContext));
-						}
-					} else {
-						if (result != null) {
-							result.setText(mContext
-									.getString(R.string.errorIncorrectGestationAge));
-						}
-
-						if (message != null) {
-							if (currentDate.before(start)) {
-								message.setText(mContext
-										.getString(R.string.errorIncorrectCurrentDateSmaller));
-							} else if (currentDate.after(end)) {
-								message.setText(mContext
-										.getString(R.string.errorIncorrectCurrentDateGreater));
-							}
-						}
-					}
-				}
+		TextView result = results[i], message = messages[i];
+		Pregnancy pregnancy = pregnancies[i];
+		Calendar start = pregnancy.getStartPoint();
+		Calendar end = pregnancy.getEndPoint();
+		if (pregnancy.isCorrect()) {
+			result.setText(pregnancy.getInfo(mContext));
+			message.setText(pregnancy.getAdditionalInfo(mContext));
+		} else {
+			result.setText(mContext
+					.getString(R.string.errorIncorrectGestationAge));
+			if (currentDate.before(start)) {
+				message.setText(mContext
+						.getString(R.string.errorIncorrectCurrentDateSmaller));
+			} else if (currentDate.after(end)) {
+				message.setText(mContext
+						.getString(R.string.errorIncorrectCurrentDateGreater));
 			}
 		}
 	}
 
 	private void setEdcTexts(int i) {
-		if (results.length == 3 && messages.length == 3
-				&& pregnancies.length == 3 && i < 3 && i >= 0) {
-			TextView result = results[i];
-			Pregnancy pregnancy = pregnancies[i];
-			if (pregnancy != null && result != null) {
-				Calendar childbirthDate = pregnancy.getEndPoint();
-				if (childbirthDate != null) {
-					result.setText(DateUtils.toString(mContext, childbirthDate));
-				}
-			}
-		}
+		TextView result = results[i];
+		Pregnancy pregnancy = pregnancies[i];
+		Calendar childbirthDate = pregnancy.getEndPoint();
+		result.setText(DateUtils.toString(mContext, childbirthDate));
 	}
 
 	private void setLast(int i) {
-		if (results.length == 3 && messages.length == 3
-				&& pregnancies.length == 3 && i >= 0 && i < 3) {
-			TextView message = messages[i];
-			Pregnancy pregnancy = pregnancies[i];
-			if (message != null && pregnancy != null) {
-				if (i == 2 && pregnancy.isCorrect()) {
-					CharSequence old = message.getText();
-					if (pregnancy.isAccurateForUltrasound(weeks)) {
-						message.setText((old.equals("") ? "" : old + "\n")
-								+ mContext
-										.getString(R.string.accurateUltrasoundResults));
-					} else {
-						message.setText((old.equals("") ? "" : old + "\n")
-								+ mContext
-										.getString(R.string.inaccurateUltrasoundResults));
-					}
-				}
-
-				if (message.getText().equals("")) {
-					message.setVisibility(View.GONE);
-				}
+		TextView message = messages[i];
+		Pregnancy pregnancy = pregnancies[i];
+		if (i == 2 && pregnancy.isCorrect()) {
+			CharSequence old = message.getText();
+			if (pregnancy.isAccurateForUltrasound(weeks)) {
+				message.setText((old.equals("") ? "" : old + "\n")
+						+ mContext
+								.getString(R.string.accurateUltrasoundResults));
+			} else {
+				message.setText((old.equals("") ? "" : old + "\n")
+						+ mContext
+								.getString(R.string.inaccurateUltrasoundResults));
 			}
+		}
+
+		if (message.getText().equals("")) {
+			message.setVisibility(View.GONE);
 		}
 	}
 
