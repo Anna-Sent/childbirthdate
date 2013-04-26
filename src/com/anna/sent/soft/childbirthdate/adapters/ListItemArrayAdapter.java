c893e86cfe34aa9be81c41675f6c68f0d28b665d
anna sent <anna.sent@gmail.com>
2013-04-26 21:09:54 +0400

    listitemarrayadapter getview method, logging;

--- src/com/anna/sent/soft/childbirthdate/adapters/ListItemArrayAdapter.java
+++ src/com/anna/sent/soft/childbirthdate/adapters/ListItemArrayAdapter.java
@@ -16,13 +16,14 @@ public class ListItemArrayAdapter extends ArrayAdapter<String> implements
 		OnClickListener {
 	private static final String TAG = "moo";
 	private static final boolean DEBUG = true;
+	private static final boolean DEBUG_CREATION = false;
 
 	private String wrapMsg(String msg) {
 		return getClass().getSimpleName() + ": " + msg;
 	}
 
-	private void log(String msg) {
-		if (DEBUG) {
+	private void log(String msg, boolean scenario) {
+		if (scenario) {
 			Log.d(TAG, wrapMsg(msg));
 		}
 	}
@@ -30,7 +31,7 @@ public class ListItemArrayAdapter extends ArrayAdapter<String> implements
 	private String[] mStrings1;
 	private String[] mStrings2;
 	private boolean[] mChecked;
-	private ViewHolder[] mViewHolders;
+	// private ViewHolder[] mViewHolders;
 	private int mCount;
 
 	private static class ViewHolder {
@@ -43,13 +44,13 @@ public class ListItemArrayAdapter extends ArrayAdapter<String> implements
 	public ListItemArrayAdapter(Context context, String[] strings1,
 			String[] strings2, boolean[] checked) {
 		super(context, R.layout.list_item, R.id.text1, strings1);
-		log("create");
+		log("create", DEBUG);
 		mStrings1 = strings1;
 		mStrings2 = strings2;
 		mChecked = checked;
 		mCount = Math.min(mStrings1.length, mStrings2.length);
 		mCount = Math.min(mCount, mChecked.length);
-		mViewHolders = new ViewHolder[mCount];
+		// mViewHolders = new ViewHolder[mCount];
 	}
 
 	@Override
@@ -59,36 +60,43 @@ public class ListItemArrayAdapter extends ArrayAdapter<String> implements
 
 	@Override
 	public View getView(int position, View contentView, ViewGroup viewGroup) {
-		log("get view " + position);
-		if (contentView != null) {
-			log("content view is " + contentView.getClass().getSimpleName());
+		log("get view " + position, DEBUG_CREATION);
+		View view;
+		ViewHolder viewHolder = null;
+		if (contentView == null) {
+			LayoutInflater layoutInflater = (LayoutInflater) getContext()
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			view = layoutInflater.inflate(R.layout.list_item, null);
+			if (view == null) {
+				log("view is not created", DEBUG_CREATION);
+			} else {
+				log("created view " + view.toString(), DEBUG_CREATION);
+				viewHolder = new ViewHolder();
+				viewHolder.checkBox = (CheckBox) view
+						.findViewById(R.id.checkBox);
+				viewHolder.text1 = (TextView) view.findViewById(R.id.text1);
+				viewHolder.text2 = (TextView) view.findViewById(R.id.text2);
+				viewHolder.position = position;
+				view.setTag(viewHolder);
+			}
 		} else {
-			log("content view is null");
+			view = contentView;
+			log("existing view " + view.toString(), DEBUG_CREATION);
+			viewHolder = (ViewHolder) view.getTag();
 		}
 
-		if (viewGroup != null) {
-			log("view group is " + viewGroup.getClass().getSimpleName());
-		} else {
-			log("view group is null");
+		if (viewHolder != null) {
+			log("update view holder", DEBUG_CREATION);
+			viewHolder.checkBox.setChecked(mChecked[position]);
+			viewHolder.checkBox.setOnClickListener(this);
+			viewHolder.checkBox.setTag(viewHolder);
+			viewHolder.text1.setText(mStrings1[position]);
+			viewHolder.text2.setText(mStrings2[position]);
+			viewHolder.text2
+					.setVisibility(viewHolder.checkBox.isChecked() ? View.VISIBLE
+							: View.GONE);
 		}
 
-		LayoutInflater layoutInflater = (LayoutInflater) getContext()
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View view = layoutInflater.inflate(R.layout.list_item, null);
-		ViewHolder viewHolder = new ViewHolder();
-		viewHolder.checkBox = (CheckBox) view.findViewById(R.id.checkBox);
-		viewHolder.text1 = (TextView) view.findViewById(R.id.text1);
-		viewHolder.text2 = (TextView) view.findViewById(R.id.text2);
-		viewHolder.position = position;
-		viewHolder.checkBox.setChecked(mChecked[position]);
-		viewHolder.checkBox.setOnClickListener(this);
-		viewHolder.checkBox.setTag(viewHolder);
-		viewHolder.text1.setText(mStrings1[position]);
-		viewHolder.text2.setText(mStrings2[position]);
-		viewHolder.text2
-				.setVisibility(viewHolder.checkBox.isChecked() ? View.VISIBLE
-						: View.GONE);
-		mViewHolders[position] = viewHolder;
 		return view;
 	}
 
@@ -104,7 +112,7 @@ public class ListItemArrayAdapter extends ArrayAdapter<String> implements
 	}
 
 	public void updateValues(String[] strings2) {
-		log("update values");
+		log("update values", DEBUG);
 		for (int i = 0; i < mCount && i < strings2.length; ++i) {
 			mStrings2[i] = strings2[i];
 		}
