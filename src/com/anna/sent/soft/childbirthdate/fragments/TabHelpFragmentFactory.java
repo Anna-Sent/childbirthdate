680c9f6a61e85e38159e71e85a96147b7615f4bb
anna sent <anna.sent@gmail.com>
2013-07-22 04:54:20 +0400

    remove logging

--- src/com/anna/sent/soft/childbirthdate/fragments/TabHelpFragmentFactory.java
+++ src/com/anna/sent/soft/childbirthdate/fragments/TabHelpFragmentFactory.java
@@ -2,7 +2,6 @@ package com.anna.sent.soft.childbirthdate.fragments;
 
 import android.os.Bundle;
 import android.text.Html;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
