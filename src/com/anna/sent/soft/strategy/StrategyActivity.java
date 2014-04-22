d5c93dcd010dec8ecd411d9d2ccfbd852510c042
anna sent <anna.sent@gmail.com>
2014-04-22 21:39:51 +0400

    strategies

--- src/com/anna/sent/soft/strategy/StrategyActivity.java
+++ src/com/anna/sent/soft/strategy/StrategyActivity.java
@@ -1,10 +1,5 @@
 package com.anna.sent.soft.strategy;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -13,8 +8,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class StrategyActivity extends FragmentActivity {
-	private Map<String, Strategy> mStrategies = new HashMap<String, Strategy>();
-	private List<String> mKeys = new ArrayList<String>();
+	private Strategies mStrategies = new Strategies();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,135 +16,84 @@ public class StrategyActivity extends FragmentActivity {
 
 		addStrategies();
 
-		for (int i = 0; i < mKeys.size(); ++i) {
-			String key = mKeys.get(i);
-			getStrategy(key).onCreate(savedInstanceState);
-		}
+		mStrategies.onCreate(savedInstanceState);
 	}
 
 	protected void addStrategies() {
 	}
 
 	protected final void addStrategy(BaseStrategy strategy) {
-		String key = strategy.getName();
-		mStrategies.put(key, strategy);
-		if (!mKeys.contains(key)) {
-			mKeys.add(key);
-		}
+		mStrategies.addStrategy(strategy);
 	}
 
 	protected final Strategy getStrategy(String name) {
-		return mStrategies.get(name);
+		return mStrategies.getStrategy(name);
 	}
 
 	@Override
 	protected void onRestart() {
 		super.onRestart();
-
-		for (int i = 0; i < mKeys.size(); ++i) {
-			String key = mKeys.get(i);
-			getStrategy(key).onRestart();
-		}
+		mStrategies.onRestart();
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
-
-		for (int i = 0; i < mKeys.size(); ++i) {
-			String key = mKeys.get(i);
-			getStrategy(key).onStart();
-		}
+		mStrategies.onStart();
 	}
 
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-
-		for (int i = 0; i < mKeys.size(); ++i) {
-			String key = mKeys.get(i);
-			getStrategy(key).onActivityResult(requestCode, resultCode, data);
-		}
+		mStrategies.onActivityResult(requestCode, resultCode, data);
 	}
 
 	@Override
 	public void onRestoreInstanceState(Bundle savedInstanceState) {
 		super.onRestoreInstanceState(savedInstanceState);
-
-		for (int i = 0; i < mKeys.size(); ++i) {
-			String key = mKeys.get(i);
-			getStrategy(key).onRestoreInstanceState(savedInstanceState);
-		}
+		mStrategies.onRestoreInstanceState(savedInstanceState);
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-
-		for (int i = 0; i < mKeys.size(); ++i) {
-			String key = mKeys.get(i);
-			getStrategy(key).onResume();
-		}
+		mStrategies.onResume();
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
-
-		for (int i = 0; i < mKeys.size(); ++i) {
-			String key = mKeys.get(i);
-			getStrategy(key).onPause();
-		}
+		mStrategies.onPause();
 	}
 
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
-		for (int i = 0; i < mKeys.size(); ++i) {
-			String key = mKeys.get(i);
-			getStrategy(key).onSaveInstanceState(outState);
-		}
-
+		mStrategies.onSaveInstanceState(outState);
 		super.onSaveInstanceState(outState);
 	}
 
 	@Override
 	protected void onStop() {
-		for (int i = 0; i < mKeys.size(); ++i) {
-			String key = mKeys.get(i);
-			getStrategy(key).onStop();
-		}
-
+		mStrategies.onStop();
 		super.onStop();
 	}
 
 	@Override
 	protected void onDestroy() {
-		for (int i = 0; i < mKeys.size(); ++i) {
-			String key = mKeys.get(i);
-			getStrategy(key).onStop();
-		}
-
+		mStrategies.onDestroy();
 		super.onDestroy();
 	}
 
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
 		super.onConfigurationChanged(newConfig);
-
-		for (int i = 0; i < mKeys.size(); ++i) {
-			String key = mKeys.get(i);
-			getStrategy(key).onConfigurationChanged(newConfig);
-		}
+		mStrategies.onConfigurationChanged(newConfig);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		for (int i = 0; i < mKeys.size(); ++i) {
-			String key = mKeys.get(i);
-			boolean processed = getStrategy(key).onCreateOptionsMenu(menu);
-			if (processed) {
-				return true;
-			}
+		if (mStrategies.onCreateOptionsMenu(menu)) {
+			return true;
 		}
 
 		return super.onCreateOptionsMenu(menu);
@@ -158,12 +101,8 @@ public class StrategyActivity extends FragmentActivity {
 
 	@Override
 	public boolean onPrepareOptionsMenu(Menu menu) {
-		for (int i = 0; i < mKeys.size(); ++i) {
-			String key = mKeys.get(i);
-			boolean processed = getStrategy(key).onPrepareOptionsMenu(menu);
-			if (processed) {
-				return true;
-			}
+		if (mStrategies.onPrepareOptionsMenu(menu)) {
+			return true;
 		}
 
 		return super.onPrepareOptionsMenu(menu);
@@ -171,12 +110,8 @@ public class StrategyActivity extends FragmentActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		for (int i = 0; i < mKeys.size(); ++i) {
-			String key = mKeys.get(i);
-			boolean processed = getStrategy(key).onOptionsItemSelected(item);
-			if (processed) {
-				return true;
-			}
+		if (mStrategies.onOptionsItemSelected(item)) {
+			return true;
 		}
 
 		return super.onOptionsItemSelected(item);
