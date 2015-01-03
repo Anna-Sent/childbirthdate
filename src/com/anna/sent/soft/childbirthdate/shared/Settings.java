6dc1852bc26bd0e4a3c0c45011407be05cf3b203
anna sent <anna.sent@gmail.com>
2015-01-03 22:18:26 +0400

    wrapper

--- src/com/anna/sent/soft/childbirthdate/shared/Settings.java
+++ src/com/anna/sent/soft/childbirthdate/shared/Settings.java
@@ -1,14 +1,10 @@
 package com.anna.sent.soft.childbirthdate.shared;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
-import android.os.Build;
 
 import com.anna.sent.soft.childbirthdate.R;
 import com.anna.sent.soft.childbirthdate.age.Age;
@@ -19,98 +15,9 @@ import com.anna.sent.soft.childbirthdate.age.SettingsParser;
 import com.anna.sent.soft.childbirthdate.sicklist.SickListConstants;
 import com.anna.sent.soft.settings.SettingsLanguage;
 import com.anna.sent.soft.settings.SettingsTheme;
+import com.anna.sent.soft.settings.SharedPreferencesWrapper;
 
 public class Settings {
-	private static class SharedPreferencesWrapper implements SharedPreferences {
-		private SharedPreferences mSettings;
-
-		public SharedPreferencesWrapper(SharedPreferences settings) {
-			mSettings = settings;
-		}
-
-		@Override
-		public Map<String, ?> getAll() {
-			return mSettings.getAll();
-		}
-
-		@Override
-		public String getString(String key, String defValue) {
-			try {
-				return mSettings.getString(key, defValue);
-			} catch (ClassCastException e) {
-				return defValue;
-			}
-		}
-
-		@TargetApi(Build.VERSION_CODES.HONEYCOMB)
-		@Override
-		public Set<String> getStringSet(String key, Set<String> defValues) {
-			try {
-				return mSettings.getStringSet(key, defValues);
-			} catch (ClassCastException e) {
-				return defValues;
-			}
-		}
-
-		@Override
-		public int getInt(String key, int defValue) {
-			try {
-				return mSettings.getInt(key, defValue);
-			} catch (ClassCastException e) {
-				return defValue;
-			}
-		}
-
-		@Override
-		public long getLong(String key, long defValue) {
-			try {
-				return mSettings.getLong(key, defValue);
-			} catch (ClassCastException e) {
-				return defValue;
-			}
-		}
-
-		@Override
-		public float getFloat(String key, float defValue) {
-			try {
-				return mSettings.getFloat(key, defValue);
-			} catch (ClassCastException e) {
-				return defValue;
-			}
-		}
-
-		@Override
-		public boolean getBoolean(String key, boolean defValue) {
-			try {
-				return mSettings.getBoolean(key, defValue);
-			} catch (ClassCastException e) {
-				return defValue;
-			}
-		}
-
-		@Override
-		public boolean contains(String key) {
-			return mSettings.contains(key);
-		}
-
-		@Override
-		public Editor edit() {
-			return mSettings.edit();
-		}
-
-		@Override
-		public void registerOnSharedPreferenceChangeListener(
-				OnSharedPreferenceChangeListener listener) {
-			mSettings.registerOnSharedPreferenceChangeListener(listener);
-		}
-
-		@Override
-		public void unregisterOnSharedPreferenceChangeListener(
-				OnSharedPreferenceChangeListener listener) {
-			mSettings.registerOnSharedPreferenceChangeListener(listener);
-		}
-	}
-
 	private static final String SETTINGS_FILE = "childbirthdatesettings";
 
 	public static SharedPreferences getSettings(Context context) {
