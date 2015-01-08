4e245ea74d3ea777e8b31ea5399dc923e299c265
anna sent <anna.sent@gmail.com>
2015-01-08 19:25:24 +0400

    ad setup

--- src/com/anna/sent/soft/childbirthdate/utils/AdUtils.java
+++ src/com/anna/sent/soft/childbirthdate/utils/AdUtils.java
@@ -1,7 +1,9 @@
 package com.anna.sent.soft.childbirthdate.utils;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.res.Configuration;
+import android.os.Build;
 import android.util.DisplayMetrics;
 
 import com.anna.sent.soft.childbirthdate.data.Data;
@@ -10,17 +12,38 @@ import com.google.ads.AdRequest.Gender;
 import com.google.ads.AdView;
 
 public class AdUtils {
+	private static int getScreenHeightDp(Activity activity) {
+		DisplayMetrics metrics = new DisplayMetrics();
+		activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+		int heightPx = metrics.heightPixels;
+		int heightDp = (int) (heightPx / metrics.density);
+		return heightDp;
+	}
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+	private static int getScreenHeightDpNew(Activity activity) {
+		int heightDp = 0;
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+			heightDp = activity.getResources().getConfiguration().screenHeightDp;
+			if (heightDp == Configuration.SCREEN_HEIGHT_DP_UNDEFINED) {
+				heightDp = 0;
+			}
+		}
+
+		if (heightDp == 0) {
+			heightDp = getScreenHeightDp(activity);
+		}
+
+		return heightDp;
+	}
+
 	public static void setupAd(Data data, Activity activity, int adViewId,
 			int initH, int rowH) {
 		int count = data.getSelectedMethodsCount();
 		int expectedHeightDp = initH + count * rowH;
 
-		DisplayMetrics metrics = new DisplayMetrics();
-		activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
-
-		int heightPx = activity.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT ? metrics.heightPixels
-				: metrics.widthPixels;
-		int actualHeightDp = (int) (heightPx / metrics.density);
+		int actualHeightDp = getScreenHeightDpNew(activity);
 
 		if (expectedHeightDp < actualHeightDp) {
 			AdView mAdView = (AdView) activity.findViewById(adViewId);
