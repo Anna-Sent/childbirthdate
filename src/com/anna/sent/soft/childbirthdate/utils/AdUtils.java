d75641b177ee745a47e60ca64935d73c09de4177
Anna Sentyakova <anna.sent@gmail.com>
2016-03-20 23:13:18 +0400

    prepare to create ad free version

--- src/com/anna/sent/soft/childbirthdate/utils/AdUtils.java
+++ src/com/anna/sent/soft/childbirthdate/utils/AdUtils.java
@@ -1,6 +1,7 @@
 package com.anna.sent.soft.childbirthdate.utils;
 
 import android.app.Activity;
+import android.content.Context;
 
 import com.anna.sent.soft.childbirthdate.data.Data;
 import com.anna.sent.soft.utils.DisplayUtils;
@@ -9,18 +10,28 @@ import com.google.ads.AdRequest.Gender;
 import com.google.ads.AdView;
 
 public class AdUtils {
+	private static boolean isAdFreeVersion(Context context) {
+		return context.getPackageName().endsWith(".pro");
+	}
+
 	public static void setupAd(Data data, Activity activity, int adViewId,
 			int initH, int rowH) {
-		int count = data.getSelectedMethodsCount();
-		int expectedHeightDp = initH + count * rowH;
+		if (!isAdFreeVersion(activity)) {
+			int count = data.getSelectedMethodsCount();
+			int expectedHeightDp = initH + count * rowH;
 
-		int actualHeightDp = DisplayUtils.getScreenSizeInDpWrapped(activity).y;
+			int actualHeightDp = DisplayUtils
+					.getScreenSizeInDpWrapped(activity).y;
 
-		if (expectedHeightDp < actualHeightDp) {
-			AdView mAdView = (AdView) activity.findViewById(adViewId);
-			AdRequest mAdRequest = new AdRequest();
-			mAdRequest.setGender(Gender.FEMALE);
-			mAdView.loadAd(mAdRequest);
+			if (expectedHeightDp < actualHeightDp) {
+				AdView adView = (AdView) activity.findViewById(adViewId);
+				if (adView != null) {
+					AdRequest adRequest = new AdRequest();
+					adRequest.addTestDevice("2600D922057328C48F2E6DBAB33639C1");
+					adRequest.setGender(Gender.FEMALE);
+					adView.loadAd(adRequest);
+				}
+			}
 		}
 	}
 }
