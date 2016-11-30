fcd4ce6fc9baf60a26f22a90e989f847153d21fc
Anna-Sentyakova <anna.sent@gmail.com>
2016-11-30 13:27:43 +0400

    log app events to Firebase Crash Reporting

--- app/src/main/java/com/anna/sent/soft/childbirthdate/base/StateSaverActivity.java
+++ app/src/main/java/com/anna/sent/soft/childbirthdate/base/StateSaverActivity.java
@@ -1,12 +1,20 @@
 package com.anna.sent.soft.childbirthdate.base;
 
+import android.util.Log;
+
 import com.anna.sent.soft.childbirthdate.R;
 import com.anna.sent.soft.childbirthdate.shared.Settings;
 import com.anna.sent.soft.strategy.statesaver.StateSaverBaseActivity;
 import com.anna.sent.soft.utils.LanguageUtils;
 import com.anna.sent.soft.utils.ThemeUtils;
+import com.google.firebase.crash.FirebaseCrash;
 
 public abstract class StateSaverActivity extends StateSaverBaseActivity {
+    @Override
+    public void log(String msg) {
+        FirebaseCrash.logcat(Log.DEBUG, getClass().getSimpleName(), msg);
+    }
+
     protected void setupTheme() {
         ThemeUtils.setupThemeBeforeOnActivityCreate(this,
                 Settings.settingsTheme.getStyle(this, R.array.style,
